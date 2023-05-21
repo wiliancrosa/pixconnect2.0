6261bc9a4ef531191f17d11202e7aaf26c9d7e78
wiliancrosa <117321491+wiliancrosa@users.noreply.github.com>
2023-05-21 22:47:04 +0100

    tentativa de adicionar uma página pop up php

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var button = document.querySelector(".cadastro-container");
+
+  button.addEventListener("click", function () {
+    window.location.href = "cadastro.php";
+  });
+});
